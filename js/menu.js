358cecfd3d68665829855c2171b77d958c92c04a
Dmytro Gobosh <irulka103@ukr.net>
2021-11-04 16:45:02 +0200

    fixed mistakes

--- js/menu.js
+++ js/menu.js
@@ -35,18 +35,6 @@
     menu: document.querySelector('[data-menu]'),
   };
 
-  // const toggleMenu = () => {
-  //   const isMenuOpen =
-  //     refs.openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-  //   refs.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-  //   refs.menu.classList.toggle('is-open');
-
-  //   const scrollLockMethod = !isMenuOpen
-  //     ? 'disableBodyScroll'
-  //     : 'enableBodyScroll';
-  //   bodyScrollLock[scrollLockMethod](document.body);
-  // };
-
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
 
